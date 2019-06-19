9b3ba97f09a6d9b9473ff9657b08b7ba1627b5ae
Vladimir Yuldashev <misterio92@gmail.com>
2019-06-20 00:14:57 +0300

    pull base image before build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build 7.3 -t misterio92/laravel:7.3
+docker build 7.3 --pull -t misterio92/laravel:7.3
 docker push misterio92/laravel:7.3
 
-docker build 7.3-nginx -t misterio92/laravel:7.3-nginx
+docker build 7.3-nginx --pull -t misterio92/laravel:7.3-nginx
 docker push misterio92/laravel:7.3-nginx
 
-docker build 7.3-scheduler -t misterio92/laravel:7.3-scheduler
+docker build 7.3-scheduler --pull -t misterio92/laravel:7.3-scheduler
 docker push misterio92/laravel:7.3-scheduler
