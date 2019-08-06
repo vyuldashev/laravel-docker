cd4ce8f518b17534920010736724f9080134e235
Vladimir Yuldashev <misterio92@gmail.com>
2019-08-07 00:08:16 +0300

    wip

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 echo "CLI"
+docker build 7.2-cli --pull -t misterio92/laravel:7.2-cli
 docker build 7.3-cli --pull -t misterio92/laravel:7.3-cli
 
 echo "FPM"
+docker build 7.2-fpm --pull -t misterio92/laravel:7.2-fpm
 docker build 7.3-fpm --pull -t misterio92/laravel:7.3-fpm
 
 echo "Scheduler"
+docker build 7.2-scheduler -t misterio92/laravel:7.2-scheduler
 docker build 7.3-scheduler -t misterio92/laravel:7.3-scheduler
 
 echo "Nginx"
