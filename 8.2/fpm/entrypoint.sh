aa52ca0dd9ac5981fc578bc10cb962d914117b40
vyuldashev <misterio92@gmail.com>
2023-01-11 12:33:56 +0400

    8.2

---
+++ 8.2/fpm/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+if [[ $CONFIG_CACHE == true ]]; then
+    php /var/www/app/artisan config:cache
+fi
+
+if [[ $ROUTE_CACHE == true ]]; then
+    php /var/www/app/artisan route:cache
+fi
+
+if [[ $VIEW_CACHE == true ]]; then
+    php /var/www/app/artisan view:cache
+fi
+
+if [[ $STORAGE_LINK == true ]]; then
+    php /var/www/app/artisan storage:link
+fi
+
+. /usr/local/bin/docker-php-entrypoint
+
+exec "$@"
